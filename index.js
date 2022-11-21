98c00eb05155b67d4fbf743c07477d2b81af5b62
densyakun <densyakun1128@gmail.com>
2022-11-22 03:39:11 +0900

    Support err fix in other modules imported

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const path = require('path')
 
 module.exports = (hmr, watchFile, callbackImported, callbackPreImport, options, callbackCatch) => {
-  const modulePath = path.resolve(path.dirname(module.parent.filename), watchFile)
+  const parentDirName = path.dirname(module.parent.filename)
+  const watchModules = options?.watchFilePatterns || [watchFile]
+
   hmr(() => {
-    if (callbackPreImport) callbackPreImport()
+    if (callbackPreImport)
+      callbackPreImport()
 
     try {
+      const modulePath = path.resolve(parentDirName, watchFile)
+
+      // Modules set in require.cache to watch for file changes by this module,
+      // may return undefined when imported by the require function, so
+      // modules to be imported must be deleted from require.cache.
+      watchModules.forEach(modulePath => {
+        delete require.cache[require.resolve(path.resolve(parentDirName, modulePath))]
+      })
+
       const importedModule = require(modulePath)
 
-      if (callbackImported) callbackImported(importedModule)
+      if (callbackImported)
+        callbackImported(importedModule)
     } catch (e) {
       callbackCatch ? callbackCatch(e) : console.error(e)
 
-      const moduleId = require.resolve(modulePath)
-      require.cache[moduleId] = { id: moduleId }
+      // If an error is thrown when importing a module, the modules is removed from require.cache.
+      // So, to watch for changes in the file, here we add a modules to require.cache.
+      watchModules.forEach(modulePath => {
+        const moduleId = require.resolve(path.resolve(parentDirName, modulePath))
+        if (!require.cache[moduleId])
+          require.cache[moduleId] = { id: moduleId } // Importing a module set here with the require function returns undefined.
+      })
     }
   }, { watchFilePatterns: [watchFile], ...options })
 }
