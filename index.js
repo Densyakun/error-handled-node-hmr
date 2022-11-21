fab32a69c7de94d71d45b18eeae7a81ed6b31a23
densyakun <densyakun1128@gmail.com>
2022-11-21 20:42:54 +0900

    fix cannot find module

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const path = require('path')
+
 module.exports = (hmr, watchFile, callbackImported, callbackPreImport, options, callbackCatch) => {
+  const modulePath = path.resolve(path.dirname(module.parent.filename), watchFile)
   hmr(() => {
     if (callbackPreImport) callbackPreImport()
 
     try {
-      const importedModule = require(watchFile)
+      const importedModule = require(modulePath)
 
       if (callbackImported) callbackImported(importedModule)
     } catch (e) {
       callbackCatch ? callbackCatch(e) : console.error(e)
 
-      const moduleId = require.resolve(watchFile)
+      const moduleId = require.resolve(modulePath)
       require.cache[moduleId] = { id: moduleId }
     }
   }, { watchFilePatterns: [watchFile], ...options })
